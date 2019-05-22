54c081b323b50dbd394b9785fbeb63d1f59180e6
yagiko0410 <iamnamake@yahoo.co.jp>
2019-05-23 00:00:53 +0900

    グループ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+  end
 end
