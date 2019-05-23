ffe2af64ca39e51d38c600204b338ac53fcc38cb
yagiko0410 <iamnamake@yahoo.co.jp>
2019-05-24 00:04:58 +0900

    メッセージ機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
   end
 end
